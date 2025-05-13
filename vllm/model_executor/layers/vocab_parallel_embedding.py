fc9e5524490f74e351ab68c3498363fdcc66da67
Kurt Chen <kurt.chen@intel.com>
2025-05-13 01:10:36 +0000

    vocab_parallel_embedding load wight WA

--- vllm/model_executor/layers/vocab_parallel_embedding.py
+++ vllm/model_executor/layers/vocab_parallel_embedding.py
@@ -388,13 +388,12 @@ class VocabParallelEmbedding(torch.nn.Module):
             # FIXME(kzawora): Weight copy with slicing bugs out on Gaudi here,
             # so we're using a workaround. Remove this when fixed in
             # HPU PT bridge.
-            #padded_weight = torch.cat([
-            #    loaded_weight,
-            #    torch.zeros(param.shape[0] - loaded_weight.shape[0],
-            #                *loaded_weight.shape[1:])
-            #])
-            #param.data.copy_(padded_weight)
-            pass
+            padded_weight = torch.cat([
+                loaded_weight.cpu(),
+                torch.zeros(param.shape[0] - loaded_weight.shape[0],
+                            *loaded_weight.shape[1:])
+            ]).to("hpu")
+            param.data.copy_(padded_weight)
         else:
             param[:loaded_weight.shape[0]].data.copy_(loaded_weight)
             param[loaded_weight.shape[0]:].data.fill_(0)
